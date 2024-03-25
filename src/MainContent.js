b7ea48e8b06cfa6a3434915056fce4dc1ce40d0b
Vinicius Herminio <viniciusherminio1@outlook.com>
2024-03-25 14:16:24 -0300

    primeiro commit projeto

---
+++ src/MainContent.js
@@ -0,0 +1,13 @@
+// MainContent.js
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <h2>Bem-vindo à nossa F5 Soluções</h2>
+      <p>Aqui você pode encontrar informações sobre nossos serviços e consultar nossos contratos disponíveis.</p>
+    </div>
+  );
+}
+
+export default MainContent;

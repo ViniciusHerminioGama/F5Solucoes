18dd3fe0c8f4bec3b92072185dd0a9ae78248c3d
Vinicius Herminio <viniciusherminio1@outlook.com>
2024-03-26 08:56:12 -0300

    ajuste site Bem vindo

--- src/MainContent.js
+++ src/MainContent.js
@@ -4,7 +4,7 @@ import React from 'react';
 function MainContent() {
   return (
     <div className="main-content">
-      <h2>Bem-vindo à nossa F5 Soluções</h2>
+      <h2>Bem-vindo à F5 Soluções</h2>
       <p>Aqui você pode encontrar informações sobre nossos serviços e consultar nossos contratos disponíveis.</p>
     </div>
   );

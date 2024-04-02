39998b8a2cd49e0086032f9aeed622226c3ca261
Vinicius Herminio <viniciusherminio1@outlook.com>
2024-04-02 13:17:03 -0300

    adicionando contratos ao site

--- src/components/ContractsPage.js
+++ src/components/ContractsPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import contrato1 from '../Contracts/CONTRATO01.pdf';
+import contrato2 from '../Contracts/CONTRATO02.pdf';
+import contrato3 from '../Contracts/CONTRATO03.pdf';
 
 function ContractsPage() {
   return (
     <div className="main-content">
-      <h2>Contratos Nossa Empresa</h2>
-      <p>Aqui irão se encontrar os contratos da empresa.</p>
+      <h2>Contratos</h2>
+      <div>
+        <a href={contrato1} target="_blank" rel="noopener noreferrer">Contrato 1</a>
+        <br />
+        <a href={contrato2} target="_blank" rel="noopener noreferrer">Contrato 2</a>
+        <br />
+        <a href={contrato3} target="_blank" rel="noopener noreferrer">Contrato 3</a>
+      </div>
     </div>
   );
 }
